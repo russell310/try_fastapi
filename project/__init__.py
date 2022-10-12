21962314ec45539ba701c16cbe9319733c2ca4ca
Hamim Al Mahdi Russell <russell310@gmail.com>
2022-10-12 23:06:13 +0600

    graphql style

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,5 @@
 import strawberry
 from fastapi import FastAPI
-from apps.user import users_router
 from project.database import init_db
 from strawberry.fastapi import GraphQLRouter
 from strawberry.schema.config import StrawberryConfig
@@ -18,7 +17,6 @@ def create_app() -> FastAPI:
     async def startup_event():
         init_db(app)
 
-    app.include_router(users_router, prefix="/api")
     app.include_router(graphql_app, prefix="/graphql")
 
     return app
