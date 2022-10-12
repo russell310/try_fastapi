7e2217f70bcf8498051cae329b3fda02d3fd1f99
Hamim Al Mahdi Russell <russell310@gmail.com>
2022-10-12 23:17:02 +0600

    update readme

--- helpers/helper.py
+++
@@ -1,5 +0,0 @@
-def get_valid_data(model_data_object, model_class):
-    data_dict = {}
-    for column in model_class._meta.fields:
-        data_dict[column] = getattr(model_data_object, column)
-    return data_dict
--- project/mutations.py
+++
@@ -1,9 +0,0 @@
-import strawberry
-from apps.user.mutations import UserMutation
-
-
-@strawberry.type
-class Mutation(
-    UserMutation
-):
-    pass
